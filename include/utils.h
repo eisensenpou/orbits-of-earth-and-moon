f07ee4452f74d145a4efd663a57e1befd7f35302
eisensenpou <fanflowphoto@gmail.com>
2025-11-14 20:50:13 -0600

    Add moon inclination constant

--- include/utils.h
+++ include/utils.h
@@ -7,10 +7,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <cmath>
+
 namespace constants {
     constexpr double G      = 6.67430e-11;
     constexpr double M_SUN  = 1.9891e30;
     constexpr double DT     = 3600;         // one hour in seconds
+    constexpr double MOON_INCLINATION = 5.145 * (M_PI / 180.0); // radians
 };
 
 
