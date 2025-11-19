80212285e00f6ade60e7afdd8971b34a1ab5724d
eisensenpou <fanflowphoto@gmail.com>
2025-11-18 23:32:22 -0600

    Add json loader module to read json input for bodies

---
+++ include/json_loader.h
@@ -0,0 +1,17 @@
+/*****************
+ * Author: Sinan Demir
+ * File: json_loader.h
+ * Date: 11/18/2025
+ * Purpose: Header file for JSON loader module.
+ ******************/
+
+#ifndef ORBIT_SIM_JSON_LOADER_H
+#define ORBIT_SIM_JSON_LOADER_H
+
+#include <string>
+#include <vector>
+#include "body.h"   // CelestialBody
+
+std::vector<CelestialBody> loadSystemFromJSON(const std::string& path);
+
+#endif // ORBIT_SIM_JSON_LOADER_H
